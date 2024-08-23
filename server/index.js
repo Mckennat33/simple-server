6f98ff50adc2f54e817607d079ceb08d463b14b2
Thomas <thomasmckenna12gmail.com>
2024-08-22 19:36:29 -0500

    fixed writing file bug

--- server/index.js
+++ server/index.js
@@ -1,1 +1,12 @@
-some content
+fs = require('fs')
+
+const content = 'content'
+
+fs.writeFile("/Users/thomasmckenna/simple-server/server/test.txt", content, err => {
+    if (err) {
+        console.log(err) 
+    } else {
+        console.log(content)
+    } 
+}); 
+
