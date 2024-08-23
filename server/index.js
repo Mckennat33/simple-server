680de5ea14feb6b2056f7f3600f3993b33789c52
Thomas <thomasmckenna12gmail.com>
2024-08-22 19:44:30 -0500

    added append file

--- server/index.js
+++ server/index.js
@@ -1,8 +1,18 @@
 fs = require('fs')
 
 const content = 'content'
+const moreContent = 'this is more content appended'
 
 fs.writeFile("/Users/thomasmckenna/simple-server/server/test.txt", content, err => {
+    if (err) {
+        console.log(err) 
+    } else {
+        console.log(content)
+    } 
+}); 
+
+
+fs.appendFile("/Users/thomasmckenna/simple-server/server/test.txt", moreContent, err => {
     if (err) {
         console.log(err) 
     } else {
