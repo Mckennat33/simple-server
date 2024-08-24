7d092820252e6de3e137505536bbb252d14410cc
Thomas <thomasmckenna12gmail.com>
2024-08-24 14:49:15 -0500

    simpler server up and running

--- server/index.js
+++ server/index.js
@@ -1,22 +1,39 @@
+
+// making a server
 fs = require('fs')
-
-const content = 'content'
-const moreContent = 'this is more content appended'
-
-fs.writeFile("/Users/thomasmckenna/simple-server/server/test.txt", content, err => {
-    if (err) {
-        console.log(err) 
-    } else {
-        console.log(content)
-    } 
-}); 
+const http = require('http')
 
 
-fs.appendFile("/Users/thomasmckenna/simple-server/server/test.txt", moreContent, err => {
-    if (err) {
-        console.log(err) 
-    } else {
-        console.log(content)
-    } 
-}); 
+const server = http.createServer((req, res) => {
+    res.write('Response from server')
+    res.end()
+})
+
+server.listen((3000), () => {
+    console.log('my server is running')
+})
+
+
+// writing files 
+// const content = 'content'
+// const moreContent = 'this is more content appended'
+
+
+
+// fs.writeFile("/Users/thomasmckenna/simple-server/server/test.txt", content, err => {
+//     if (err) {
+//         console.log(err) 
+//     } else {
+//         console.log(content)
+//     } 
+// }); 
+
+
+// fs.appendFile("/Users/thomasmckenna/simple-server/server/test.txt", moreContent, err => {
+//     if (err) {
+//         console.log(err) 
+//     } else {
+//         console.log(moreContent)
+//     } 
+// }); 
 
