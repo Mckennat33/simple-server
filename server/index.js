7f60537a8f122b51e1908571e5c174aca5be5fbf
Thomas <thomasmckenna12gmail.com>
2024-09-05 20:14:00 -0500

    test adding notes

--- server/index.js
+++ server/index.js
@@ -14,26 +14,5 @@ server.listen((3000), () => {
 })
 
 
-// writing files 
-// const content = 'content'
-// const moreContent = 'this is more content appended'
-
-
-
-// fs.writeFile("/Users/thomasmckenna/simple-server/server/test.txt", content, err => {
-//     if (err) {
-//         console.log(err) 
-//     } else {
-//         console.log(content)
-//     } 
-// }); 
-
-
-// fs.appendFile("/Users/thomasmckenna/simple-server/server/test.txt", moreContent, err => {
-//     if (err) {
-//         console.log(err) 
-//     } else {
-//         console.log(moreContent)
-//     } 
-// }); 
+// test notes
 
