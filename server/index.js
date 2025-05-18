71fd04a8681380e76647ec299956e515fafb91ef
tom mckenna <thomasmckenna12@gmail.com>
2025-05-18 15:36:01 -0500

    init for route and controller files

--- server/index.js
+++ server/index.js
@@ -1,12 +1,11 @@
 require('dotenv').config()
 const express = require('express')
 const app = express()
-
+const aboutRoute = require('../routes/about.route.js')
 const PORT = process.env.PORT || 5000
 
-app.get('/', (req, res) => {
-    res.send('Hello, World!')
-})
+app.get('/')
+app.get('/about', aboutRoute)
 
 app.listen(PORT, () => {
     console.log(`Listening on ${PORT}`)
