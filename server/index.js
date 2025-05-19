405d99f537348c20ce0025a4a7d6767b8925fb60
tom mckenna <thomasmckenna12@gmail.com>
2025-05-19 18:58:17 -0500

    home page

--- server/index.js
+++ server/index.js
@@ -4,7 +4,9 @@ const app = express()
 const aboutRoute = require('../routes/about.route.js')
 const PORT = process.env.PORT || 5000
 
-app.get('/')
+app.get('/', (req, res) => {
+    res.send('This is the home page')
+})
 app.get('/about', aboutRoute)
 
 app.listen(PORT, () => {
